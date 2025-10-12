6c85e5823d37f2b537fddc66e88ecd7b29703d0d
Patrusheva Anna <petrushkanya@gmail.com>
2025-10-12 12:39:45 +0300

    ok

---
+++ src/components/Middle/Middle.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Middle = () => {
+    return (
+        <div className="body">
+            Lorem ipsxum dolor sit amet, consectetur adipisicing elit. Cupiditate ducimus enim facere impedit nobis,
+            nulla placeat quam suscipit unde voluptatibus.
+
+        </div>
+
+    );
+};
+
+export default Middle;
